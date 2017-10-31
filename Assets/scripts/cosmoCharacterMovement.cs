05a10da98e6d80303bc5f4e949f44953874e71a0
userH309 <eandresen1989@gmail.com>
2017-10-31 18:54:01 +0100

    Initial commit

---
+++ Assets/scripts/cosmoCharacterMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cosmoCharacterMovement : MonoBehaviour 
+{	
+	public float maxSpeed =15f;	
+	Animator anim;
+	Rigidbody2D body;
+	GameObject spotlight;
+
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+		body = GetComponent<Rigidbody2D> ();
+		spotlight = GameObject.Find ("spotlight");
+	}
+
+	void FixedUpdate () 
+	{
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveY = Input.GetAxis ("Vertical");
+		
+		anim.SetFloat ("speedX", (moveX));
+		anim.SetFloat ("speedY", (moveY));
+
+		body.velocity = new Vector2 (moveX * maxSpeed, body.velocity.y);
+		body.velocity = new Vector2 (body.velocity.x, moveY * maxSpeed);
+	
+		if (moveX > 0)
+		{			
+			Debug.Log ("høyre");
+			spotlight.transform.rotation = Quaternion.Euler(0,90,0);
+		}
+
+		if (moveX < 0)
+		{		
+			Debug.Log ("venstre");
+			spotlight.transform.rotation = Quaternion.Euler(0,-90,0);
+		}
+
+		if (moveY > 0)
+		{
+			spotlight.transform.rotation = Quaternion.Euler(-90,0,0);
+			Debug.Log ("opp");
+		}
+
+		if (moveY < 0) 
+		{
+			spotlight.transform.rotation = Quaternion.Euler(90,0,0);
+			Debug.Log ("ned");
+		}
+
+	}	
+}
---
+++ Assets/scripts/layerTrigger.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class layerTrigger : MonoBehaviour 
+{
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		var layerArray = GameObject.FindGameObjectsWithTag("objects");
+		Debug.Log ("bak");
+
+		foreach(GameObject n in layerArray)
+		{
+			var layer = n.GetComponent<SpriteRenderer>();
+			layer.sortingOrder = 3;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		var layerArray = GameObject.FindGameObjectsWithTag("objects");
+		Debug.Log ("forann");
+
+		foreach(GameObject n in layerArray)
+		{
+			var layer = n.GetComponent<SpriteRenderer>();
+			layer.sortingOrder = 1;
+		}
+	}
+}
